788ddd87910725ecfab8fc57aafdd436cf113762
T5ive <41800233+T5ive@users.noreply.github.com>
2025-05-19 04:56:29 +0700

    Add dev gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ const router = createRouter({
 	scrollRestoration: true,
 	defaultStructuralSharing: true,
 	defaultPreloadStaleTime: 0,
+	basepath: "/last-survival-guide/",
 });
 
 // Register the router instance for type safety
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/last-survival-guide/",
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
   test: {
     globals: true,
