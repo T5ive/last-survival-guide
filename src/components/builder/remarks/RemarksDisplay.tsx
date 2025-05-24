14164a97c75054f58a335d8c554e2a7e9b17a90b
T5ive <41800233+T5ive@users.noreply.github.com>
2025-05-24 18:30:34 +0700

    Add Note in Panel Skill

--- src/components/builder/remarks/RemarksDisplay.tsx
+++ src/components/builder/remarks/RemarksDisplay.tsx
@@ -1,4 +1,3 @@
-import { useLanguage } from "@/context/LanguageContext";
 import type { Skill } from "@/types/Skill";
 
 interface RemarksDisplayProps {
@@ -7,7 +6,6 @@ interface RemarksDisplayProps {
 }
 
 const RemarksDisplay: React.FC<RemarksDisplayProps> = ({ skillsInPanel, remarks }) => {
-	const { t } = useLanguage();
 
 	const skillsWithRemarks = skillsInPanel
 		.map((skill, index) => ({
@@ -17,17 +15,38 @@ const RemarksDisplay: React.FC<RemarksDisplayProps> = ({ skillsInPanel, remarks 
 		}))
 		.filter((item) => item.skill && item.remark && item.remark.trim() !== "");
 
-	if (skillsWithRemarks.length === 0) {
-		return <div className="text-sm text-muted-foreground italic text-center py-2">{t("noRemarksAdded")}</div>;
-	}
-
 	return (
 		<div className="space-y-2 mb-4 p-3 bg-muted/50 rounded-lg border border-dashed">
 			{/* <h3 className="text-sm font-semibold mb-1">{t("remarkTab")}</h3> // Optional title for this section */}
+			<div className="flex items-start text-sm">
+				<span
+					className="w-5 h-5 mr-2 
+							flex items-center justify-center
+							rounded-full bg-red-500 text-white text-sm"
+				>
+					★
+				</span>
+				<div>
+					<span className="font-semibold mr-5">Array</span>
+				</div>
+				<span
+					className="w-5 h-5 mr-2 
+							flex items-center justify-center
+							rounded-full bg-purple-500 text-white text-sm"
+				>
+					★
+				</span>
+				<div>
+					<span className="font-semibold">Grimoire</span>
+				</div>
+			</div>
+
+			{skillsWithRemarks.length > 0 && <hr className="my-2" />}
+
 			{skillsWithRemarks.map(({ skill, remark }, displayIndex) => (
-				<div key={`remark-display-${skill!.id}-${displayIndex}`} className="flex items-start text-sm">
+				<div key={`remark-display-${skill!.id}-${displayIndex}`} className="flex items-center text-sm">
 					<img src={skill!.imageUrl} alt={skill!.name} className="w-8 h-8 object-contain mr-2 flex-shrink-0 rounded" />
-					<div>
+					<div className="align-text-bottom">
 						<span className="font-semibold">{skill!.name}:</span>
 						<span className="ml-1 text-muted-foreground break-words">{remark}</span>
 					</div>
